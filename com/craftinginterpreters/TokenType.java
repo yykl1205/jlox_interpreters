f23a5c7858d8bcf3fb03959b25dac9d510affdb2
yuyangl <yuyangl@chalemrs.se>
2025-12-19 11:05:12 +0100

    scanner01

---
+++ com/craftinginterpreters/TokenType.java
@@ -0,0 +1,20 @@
+package com.crafgtinginterpreters.lox;
+
+enum TokenType {
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+    BANG, BANG_EQUAL,
+    EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL,
+    LESS, LESS_EQUAL,
+
+    // Literals
+    IDENTIFIER, STRING, NUMER,
+
+    // Keywords
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    EOF
+}
